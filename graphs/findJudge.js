a3f72f5c934b0fcf8f886276e476a5afcb8e0912
Tommy Han <tommy@Tommys-Air.home>
2020-04-23 01:22:27 -0400

    trusted pairs

--- graphs/findJudge.js
+++ graphs/findJudge.js
@@ -1,3 +1,6 @@
+//Graph concepts
+//Edge - shows different connections between nodes.
+//Adjacent List - where index is the node and the value is the node neighbors.
 class Graph {
     constructor() {
         this.adjacentList = {};
@@ -5,7 +8,6 @@ class Graph {
 
     addNode(node){
         this.adjacentList[node] = [];
-        this.numberOfNodes++;
     }
 
     // edge is the link betwen nodes
@@ -14,14 +16,16 @@ class Graph {
         this.adjacentList[node1].push(node2)
     }
 
-    // judge doesnt have any adjacent connections
+    // judge doesnt trust so no adjacent connections(or no trusted neighbors)
+    // index is the neighbor and value is the node neighbors
     findJudge(){
-       return Object.keys(this.adjacentList).reduce((acc,currKey) =>{
-            if (this.adjacentList[currKey] == ""){
-                return currKey;
+       const judgeNotFound = -1;
+       return Object.keys(this.adjacentList).reduce((acc,currNeighbor) =>{
+            if (this.adjacentList[currNeighbor] == ""){ // no trusted neighbors
+                return currNeighbor; // non trusting judge
             }
             return acc;
-        }, -1)
+        }, judgeNotFound)
     }
 
     // display the node and connections
@@ -33,27 +37,28 @@ class Graph {
             for (let adjacentNode of adjacentNodes){
                 connections += adjacentNode + " ";
             }
-            console.log(node+ "-->" + connections);
+            console.log( node + "-->" + connections);
         }
     }
 
 
 }
 
-function findJudge(nodes, adjacencyArray){
+function findJudge(numOfNeighbors, trustPairList){
     const myGraph = new Graph();
-    for (let i=1; i<=nodes; i++){
-        myGraph.addNode(i);
+    for (let neighbor=1; neighbor<=numOfNeighbors; neighbor++){
+        myGraph.addNode(neighbor);
     }
-    adjacencyArray.forEach ( currArr =>{
-        myGraph.addEdge(...currArr);
+    trustPairList.forEach(currTrustPair =>{
+        myGraph.addEdge(...currTrustPair);
     })
     myGraph.showConnections();
     return myGraph.findJudge();
 }
 
-console.log(`nonAdjacent Judge is ${findJudge(2,[[1,2]])}\n`);  //2
-console.log(`nonAdjacent Judge is ${findJudge(3,[[1,3],[2,3]])}\n`);  //3
-console.log(`nonAdjacent Judge is ${findJudge(3,[[1,3],[2,3],[3,1]])}\n`);  //-1
-console.log(`nonAdjacent Judge is ${findJudge(3,[[1,2],[2,3]])}\n`);   //3
-console.log(`nonAdjacent Judge is ${findJudge(4,[[1,3],[1,4],[2,3],[2,4],[4,3]])}\n`); //3
+// Judge doesnt trust so he is the onle one who doesnt have any adjacent links
+console.log(`nonTrusting Judge is ${findJudge(2,[[1,2]])}\n`);  //2
+console.log(`nonTrusting Judge is ${findJudge(3,[[1,3],[2,3]])}\n`);  //3
+console.log(`nonTrusting Judge is ${findJudge(3,[[1,3],[2,3],[3,1]])}\n`);  //-1
+console.log(`nonTrusting Judge is ${findJudge(3,[[1,2],[2,3]])}\n`);   //3
+console.log(`nonTrusting Judge is ${findJudge(4,[[1,3],[1,4],[2,3],[2,4],[4,3]])}\n`); //3
